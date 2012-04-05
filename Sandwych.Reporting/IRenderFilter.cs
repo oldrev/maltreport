5a67abed02357e80ac08273b13fbef06bb697f01
Wei "oldrev" Li <oldrev@gmail.com>
2012-04-05 20:24:06 +0800

    JODConverter Added

---
+++ Sandwych.Reporting/IRenderFilter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sandwych.Reporting
+{
+	internal interface IRenderFilter
+	{
+		object Filter (object originalValue);
+	}
+}
