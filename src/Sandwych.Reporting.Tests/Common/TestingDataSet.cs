88a23a8e019f74af92306065fd360b76650f2f10
oldrev <oldrev@gmail.com>
2017-07-18 11:00:32 +0800

    add null instance for testing

--- src/Sandwych.Reporting.Tests/Common/TestingDataSet.cs
+++ src/Sandwych.Reporting.Tests/Common/TestingDataSet.cs
@@ -9,6 +9,9 @@ namespace Sandwych.Reporting.Tests.Common
 
         public SimpleObject SimpleObject { get; private set; } = new SimpleObject();
 
+        public SimpleObject NullSimpleObject { get; private set; } = null;
+
         public Table Table1 { get; private set; } = new Table();
+
     }
 }
