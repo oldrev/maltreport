6d20c83fc75473f207bea3901c51e807f80322a2
Wei "oldrev" Li <liwei@sandwych.com>
2017-07-20 12:44:51 +0800

    more tests added

--- src/Sandwych.Reporting.Tests/FluidTest.cs
+++ src/Sandwych.Reporting.Tests/FluidTest.cs
@@ -40,6 +40,31 @@ namespace Sandwych.Reporting.Tests
             Assert.Equal($"Hello {model.Str1} {model.Str2} [{model.Numbers[0].Number}{model.Numbers[1].Number}]", result);
         }
 
+        [Fact(DisplayName ="Fluid with Dynamic Object")]
+        public void FluidShouldWorksWithDynamicObject()
+        {
+            dynamic model = new
+            {
+                Str1 = "Fluid",
+                Str2 = "Template",
+                Numbers = new Item[]
+                {
+                    new Item { Number = 1 },
+                    new Item { Number = 2 }
+                }
+            };
+
+            var source = "Hello {{p.Str1}} {{ p.Str2 }} [{% for i in p.Numbers %}{{i.Number}}{% endfor %}]";
+            Assert.True(FluidTemplate.TryParse(source, out var template));
+            var context = new TemplateContext();
+            System.Diagnostics.Debug.WriteLine("{0}", model.Numbers.GetType().ToString() as string);
+            context.SetValue("p", model);
+            var result = template.Render(context);
+
+            Assert.Equal($"Hello {model.Str1} {model.Str2} [{model.Numbers[0].Number}{model.Numbers[1].Number}]", result);
+
+        }
+
 
     }
 }
---
+++ src/Sandwych.Reporting/Textilize/IFluidTemplateContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fluid;
+
+namespace Sandwych.Reporting.Textilize
+{
+    public interface IFluidTemplateContextFactory
+    {
+        TemplateContext CreateTemplateContext();
+    }
+}
