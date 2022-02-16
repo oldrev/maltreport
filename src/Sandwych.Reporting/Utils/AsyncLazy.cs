6cab5332a30ee4a3726d43d4f4cdfcd021376b36
Li Wei <oldrev@gmail.com>
2022-02-16 12:34:31 +0800

    add asyncLazy

---
+++ src/Sandwych.Reporting/Utils/AsyncLazy.cs
@@ -0,0 +1,22 @@
+// https://devblogs.microsoft.com/pfxteam/asynclazyt/
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandwych.Reporting.Utils
+{
+    public class AsyncLazy<T> : Lazy<Task<T>>
+    {
+        public AsyncLazy(Func<T> valueFactory) :
+            base(() => Task.Factory.StartNew(valueFactory))
+        { }
+
+        public AsyncLazy(Func<Task<T>> taskFactory) :
+            base(() => Task.Factory.StartNew(() => taskFactory()).Unwrap())
+        { }
+
+        public TaskAwaiter<T> GetAwaiter() { return Value.GetAwaiter(); }
+    }
+}
