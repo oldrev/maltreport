d5bb26683258889092013663f98a4db55ccf5ae1
Li Wei <oldrev@gmail.com>
2022-02-19 00:23:34 +0800

    bug: register to static MemberAccessStrategy

--- src/Sandwych.Reporting/Textilize/FluidTemplateContext.cs
+++ src/Sandwych.Reporting/Textilize/FluidTemplateContext.cs
@@ -5,16 +5,23 @@ namespace Sandwych.Reporting.Textilize
 {
     public class FluidTemplateContext : Fluid.TemplateContext
     {
-        public FluidTemplateContext(IReadOnlyDictionary<string, object> context)
+
+        public FluidTemplateContext(IReadOnlyDictionary<string, object> context, bool allowUnsafe = true)
         {
-            this.Options.MemberAccessStrategy = UnsafeMemberAccessStrategy.Instance;
+            //TODO FIXME
+            if (allowUnsafe)
+            {
+                this.Options.MemberAccessStrategy = UnsafeMemberAccessStrategy.Instance;
+            }
             foreach (var pair in context)
             {
                 this.SetValue(pair.Key, Fluid.Values.FluidValue.Create(pair.Value, this.Options));
+                /*
                 if (pair.Value != null)
                 {
                     this.Options.MemberAccessStrategy.Register(pair.Value.GetType());
                 }
+                */
             }
         }
     }
